849b2d5ab0aa81a7fba9c96cb549e7eca3fb1ea4
Jhosep Rolando Espinoza Cortez <jespinoza@cendrassos.net>
2023-10-23 16:11:54 +0200

    content-apartament

--- src/views/content.php
+++ src/views/content.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js"></script>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="icon" type="image/png" href="/public/img/logo_renting.png">
     <link rel="stylesheet" href="/public/css/style.css">
     <title>Content</title>
@@ -13,7 +16,102 @@
 <body>
     <?php include("header.php"); ?>
 
+<div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="/public/img/casa1.jpg" class="d-block w-100" alt="hola">
+    </div>
+    <div class="carousel-item">
+      <img src="/public/img/casa2.jpg" class="d-block w-100" alt="pi">
+    </div>
+    <div class="carousel-item">
+      <img src="/public/img/casa3.jpg" class="d-block w-100" alt="pe">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+<div>
+    
+<!--Slider -->
+<div class="row me-0 ms-3">
+  <!-- start col-8 -->
+    <div class="col-8"> 
+        <div class="col-12">
+            <p class="text-white display-5 ">345000€</p>
+        </div>
+        <div class="container-text">
+            <p class="text-white">
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
+            </p>
+        </div>
+        <div class="col-12">
+            <p class="text-white display-4">Caracteristicas</p>
+        </div>
+        <div class="row col-6">
+            <div class="col-3">
+                <i class="fa-solid fa-person-swimming" style="color: #ffffff;"></i>
+            </div>
+            <div class="col-3">
+                <i class="fa-solid fa-wifi" style="color: #ffffff;"></i>
+            </div>
+            <div class="col-3">
+                <i class="fa-solid fa-square-parking" style="color: #ffffff;"></i>
+            </div>
+            <div class="col-3">
+                <i class="fa-solid fa-temperature-quarter" style="color: #ffffff;"></i>
+            </div>
+        </div>
+        <!-- Google map -->
+        <div class="row mt-3">
+            <div class="col-12">
+                <div id="map-container-google-1 " class="z-depth-1-half map-container mb-3" style="height: 500px " >
+                    <iframe src="https://maps.google.com/maps?q=manhattan&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0"
+                        style="border:0; width: 100%; height: 100%;" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<!-- Form -->
+    <div class="col-3 mt-2">
+    <form action="https://www.fotocasa.es/es/" method="POST">
+        <div class="row gx-5 gy-4 text-center justify-content-center">
+            <div class="col-md-8 shadow-lg">
+                <input type="text" class="form-control-plaintext" id="user-name" placeholder="Mail">
+            </div>
+            <div class="col-md-8 shadow-lg">
+                <input type="text" class="form-control-plaintext" id="user-name" placeholder="Telefon"> 
+            </div>
+            <div class="mb-3">
+                                    <label for="from">Data Inici</label>
+                                    <input type="text" id="from" name="from" class="form-control mb-3">
+                                    <label for="to">Data Final</label>
+                                    <input type="text" id="to" name="to" class="form-control">
+                                </div>
+            <button type="submit" class="btn btn-primary btn-lg btn-block shadow-lg col-md-8">Reservar</button>
+        </div>
+    </form>   
+    </div>
+</div>
+        
+
+        
+        
+
+
+
+
+
     <?php include("footer.php"); ?>
+   
     <script src="/public/script/script.js"></script>
+    
 </body>
 </html>
+
