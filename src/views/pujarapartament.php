2e3c74020df3768ae0478c36a30a407c06eff4bf
aescribano <aescribano@cendrassos.net>
2023-10-25 16:38:42 +0200

    Fixed-Label

--- src/views/pujarapartament.php
+++ src/views/pujarapartament.php
@@ -84,21 +84,21 @@
                     </div>
                 </div>
                 <div class="col-md-5 shadow-lg border border-dark bg-light border-2 rounded-3">
-                    <label for="from">Data Inici Temporada Alta</label>
+                    <label for="data-inici-alta">Data Inici Temporada Alta</label>
                     <input type="text" id="data-inici-alta" name="data-ini-alta" class="form-control" placeholder="Data Inici Temporada Alta" value="01/07/2023">
                 </div>
                 <div class="col-md-1"></div>
                 <div class="col-md-5 shadow-lg border border-dark bg-light border-2 rounded-3">
-                    <label for="from">Data Final Temporada Alta</label>
+                    <label for="data-final-alta">Data Final Temporada Alta</label>
                     <input type="text" id="data-final-alta" name="data-fin-alta" class="form-control" placeholder="Data Final Temporada Alta" value="31/12/2023">
                 </div>
                 <div class="col-md-5 shadow-lg border border-dark bg-light border-2 rounded-3">
-                    <label for="from">Data Inici Temporada Baixa</label>
+                    <label for="data-inici-baixa">Data Inici Temporada Baixa</label>
                     <input type="text" id="data-inici-baixa" name="data-ini-baixa" class="form-control" placeholder="Data Inici Temporada Baixa" value="01/01/2023">
                 </div>
                 <div class="col-md-1"></div>
                 <div class="col-md-5 shadow-lg border border-dark bg-light border-2 rounded-3">
-                    <label for="from">Data Final Temporada Baixa</label>
+                    <label for="data-final-baixa">Data Final Temporada Baixa</label>
                     <input type="text" id="data-final-baixa" name="data-fin-baixa" class="form-control" placeholder="Data Final Temporada Baixa" value="30/06/2023">
                 </div>
                  <div class="col-md-4 shadow-lg border border-dark bg-light border-2 rounded-3">
