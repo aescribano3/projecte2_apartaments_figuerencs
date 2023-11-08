732b3930e0020640bb3f982af9b6d42429816437
aescribano <aescribano@cendrassos.net>
2023-11-08 15:49:20 +0100

    Fixed-Date-UploadApt

--- src/controllers/pujarapartament.php
+++ src/controllers/pujarapartament.php
@@ -1,7 +1,11 @@
 <?php
 
 function ctrlPujar($request, $response, $container){
-    
+
+    $currentYear = date("Y");
+
+    $response->set("currentYear", $currentYear);
+
     $response->setTemplate("pujarapartament.php");
 
     return $response;
